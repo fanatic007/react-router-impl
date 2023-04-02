52bfb714cd29c06ba8d8045cd60d97d004613d67
Sarang Tayde <sarang.tayde@gmail.com>
2023-04-03 02:18:33 +0530

    selected link styling

--- src/components/nested-list-item.tsx
+++ src/components/nested-list-item.tsx
@@ -1,5 +1,5 @@
-import { Collapse, Divider, Icon, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
-import { useState } from "react";
+import { Collapse, Divider, Icon, ListItem, ListItemButton, ListItemIcon, ListItemText, Theme, createStyles, makeStyles } from "@mui/material";
+import { PropsWithChildren, forwardRef, useState } from "react";
 import { NavLink } from "react-router-dom";
 import { INavRoute } from "../model/INavRoute";
 
@@ -8,14 +8,28 @@ import InboxIcon from '@mui/icons-material/MoveToInbox';
 import { Children } from "react";
 import CustomIcon from "./custom-icon";
 
+const NavLinkComponent = forwardRef<any, any>(({children,to,className, activeClassName}:PropsWithChildren&{to:string, className:string, activeClassName:string},ref) =>(
+    <NavLink 
+      to={to} 
+      ref={ref}
+      className={({ isActive }) => `${className} ${isActive ? activeClassName : ''}`}    
+      >
+      {children}
+    </NavLink>
+  )
+);
 
 const NestedListItem = ({route}:{route:INavRoute}) => {
   const [open, setOpen] = useState(true);
+
   return(
     <>      
       <Divider />
       <ListItemButton
-          onClick={()=>setOpen(open=>!open)} component={NavLink} to={route.path}>
+          sx={{
+            color: 'primary',
+          }}
+          onClick={()=>setOpen(open=>!open)} component={NavLinkComponent} to={route.path} activeClassName={'active-link'} >
           <CustomIcon color="primary" iconName={route.icon}></CustomIcon>
           <ListItemText primary={route.name} />
           {
--- src/pages/projects/list/projects-list.tsx
+++ src/pages/projects/list/projects-list.tsx
@@ -5,7 +5,7 @@ const ProjectsList = () => {
   const projects = [{id:'123'},{id:'2146'},{id:'42'}]
   return(
     <List>
-      { Children.toArray(projects.map(({id},i)=><ListItem><Link underline="hover" href={`${id}`}>{`${i+1}. Project ${id}`}</Link></ListItem>)) }
+      { Children.toArray(projects.map(({id},i)=><ListItem>{`${i+1}. `} <Link underline="hover" href={`${id}`}>{`Project ${id}`}</Link></ListItem>)) }
     </List>
   )
 }
